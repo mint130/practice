4a0bacc2f3058fe8dbcb89f3090612ebd4318b5d
김민주 <mintminju130@naver.com>
2026-02-04 14:18:31 +0900

    feat: 입력값 예외 처리 추가

--- src/main/java/com/example/practice/common/exception/ErrorCode.java
+++ src/main/java/com/example/practice/common/exception/ErrorCode.java
@@ -10,7 +10,9 @@ public enum ErrorCode {
     MEMBER_NOT_FOUND(NOT_FOUND, "회원을 찾을 수 없습니다."),
     MEMBER_EMAIL_DUPLICATED(CONFLICT, "이미 존재하는 이메일입니다."),
 
-    TODO_NOT_FOUND(NOT_FOUND, "할 일을 찾을 수 없습니다.");
+    TODO_NOT_FOUND(NOT_FOUND, "할 일을 찾을 수 없습니다."),
+
+    INVALID_INPUT_VALUE(BAD_REQUEST, "잘못된 입력값입니다.");
     private final HttpStatus httpStatus;
     private final String message;
 }
--- src/main/java/com/example/practice/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/practice/common/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.example.practice.common.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -18,5 +19,17 @@ public class GlobalExceptionHandler {
                 .message(errorCode.getMessage())
                 .build();
         return ResponseEntity.status(errorCode.getHttpStatus()).body(response);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException e) {
+        log.error("ValidationException: {}", e.getMessage());
+        String message = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+        ErrorResponse response = ErrorResponse.builder()
+                .errorCode(ErrorCode.INVALID_INPUT_VALUE)
+                .message(message)
+                .build();
+        return ResponseEntity.status(ErrorCode.INVALID_INPUT_VALUE.getHttpStatus()).body(response);
+
     }
 }
